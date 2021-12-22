50c62d93a0be87cbdb58bf3da916d1c2941ed375
ian <highmeme10@gmail.com>
2021-12-22 15:00:32 +0300

    Can be trained

--- main.py
+++ main.py
@@ -102,48 +102,13 @@ with mp_holistic.Holistic(
 
 """
 
+
 def extract_keypoints(results):
     # Get the values for each landmark and error handle to get zeros if a human feature is not detected
-    pose = (
-        np.array(
-            [
-                [res.x, res.y, res.z, res.visibility]
-                for res in results.pose_landmarks.landmark
-            ]
-        ).flatten
-        if results.pose_landmarks
-        else np.zeros(132)
-    )
-    face = (
-        np.array(
-            [
-                [res.x, res.y, res.z, res.visibility]
-                for res in results.face_landmarks.landmark
-            ]
-        ).flatten
-        if results.face_landmarks
-        else np.zeros(1404)
-    )
-    lh = (
-        np.array(
-            [
-                [res.x, res.y, res.z, res.visibility]
-                for res in results.left_hand_landmarks.landmark
-            ]
-        ).flatten
-        if results.left_hand_landmarks
-        else np.zeros(21 * 3)
-    )
-    rh = (
-        np.array(
-            [
-                [res.x, res.y, res.z, res.visibility]
-                for res in results.right_hand_landmarks.landmark
-            ]
-        ).flatten
-        if results.right_hand_landmarks
-        else np.zeros(21 * 3)
-    )
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose, face, lh, rh])
 
 
@@ -158,17 +123,15 @@ no_sequences = 30
 # 30fps for each sequence
 sequence_length = 30
 
-# Folder start
-start_folder = 30
-
 # Makes a directory for each action to be detected with 30 different data points
-for action in actions: 
-    dirmax = np.max(np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int))
-    for sequence in range(1,no_sequences+1):
-        try: 
-            os.makedirs(os.path.join(DATA_PATH, action, str(dirmax+sequence)))
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
         except:
             pass
+
+
 
 # Set mediapipe model
 with mp_holistic.Holistic(
