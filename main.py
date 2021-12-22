c8da500d361f779e49a9098cd787e735d413b937
ian <highmeme10@gmail.com>
2021-12-22 14:40:30 +0300

    Data collection init

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ def draw_styled_landmarks(image, results):
 
 # Access the webcam which has the value of 0
 cap = cv2.VideoCapture(0)
+"""
 
 # Set mediapipe model
 with mp_holistic.Holistic(
@@ -99,6 +100,7 @@ with mp_holistic.Holistic(
     cap.release()
     cv2.destroyAllWindows()
 
+"""
 
 def extract_keypoints(results):
     # Get the values for each landmark and error handle to get zeros if a human feature is not detected
@@ -144,21 +146,100 @@ def extract_keypoints(results):
     )
     return np.concatenate([pose, face, lh, rh])
 
-#Path for exported data, numpy arrays
+
+# Path for exported data, numpy arrays
 DATA_PATH = os.path.join("MP_DATA")
 
-#Actions that the code will try to detect
-actions = np.array(['hello', 'thanks', 'iloveyou'])
+# Actions that the code will try to detect
+actions = np.array(["hello", "thanks", "iloveyou"])
 
-#Thirty Videos worth of data
+# Thirty Videos worth of data
 no_sequences = 30
-#30fps for each sequence 
+# 30fps for each sequence
 sequence_length = 30
 
-#Makes a directory for each action to be detected with 30 different data points
-for action in actions:
-    for sequence in range(no_sequences):
-        try:
-            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+# Folder start
+start_folder = 30
+
+# Makes a directory for each action to be detected with 30 different data points
+for action in actions: 
+    dirmax = np.max(np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int))
+    for sequence in range(1,no_sequences+1):
+        try: 
+            os.makedirs(os.path.join(DATA_PATH, action, str(dirmax+sequence)))
         except:
             pass
+
+# Set mediapipe model
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5
+) as holistic:
+    # Loop through each action
+    for action in actions:
+        # Loop through each sequence
+        for sequence in range(no_sequences):
+
+            # Loop through each frame(30fps)
+            for frame_num in range(sequence_length):
+
+                # Read the frame from the webcam
+                ret, frame = cap.read()
+
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+                print(results)
+
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+
+                if frame_num == 0:
+                    cv2.putText(
+                        image,
+                        "STARTING COLLECTION",
+                        (120, 200),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (0, 255, 0),
+                        1,
+                        cv2.LINE_AA,
+                    )
+                    cv2.putText(
+                        image,
+                        f"Collecting frames for {action} video Number {sequence}",
+                        (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (0, 0, 255),
+                        1,
+                        cv2.LINE_AA,
+                    )
+                    # Show the feed with the specified frame title
+                    cv2.imshow("OpenCV Feed", image)
+                    # Wait for two seconds
+                    cv2.waitKey(2000)
+                else:
+                    cv2.putText(
+                        image,
+                        f"Collecting frames for {action} video Number {sequence}",
+                        (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (0, 0, 255),
+                        1,
+                        cv2.LINE_AA,
+                    )
+                    # Show the feed with the specified frame title
+                    cv2.imshow("OpenCV Feed", image)
+
+                keypoints = extract_keypoints(results)
+
+                #Save the keypoints in their respective action folder
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+
+                # If q is pressed exit
+                if cv2.waitKey(10) & 0xFF == ord("q"):
+                    break
+
+    cap.release()
+    cv2.destroyAllWindows()
